36cb053f17868b86b6b1d1df1ae97a4c116cefd9
vagnero <vagnerimperador16@gmail.com>
2024-10-14 16:41:09 -0300

    Header e Footer da aplicacao

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-500 w-full py-4 text-center fixed text-white bottom-0 left-0 z-10">
+        &copy; {new Date().getFullYear()} TalkConnect. Todos os direitos reservados.
+    </footer>
+    )
+}
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="bg-blue-500 w-full py-4 shadow-md fixed top-0 left-0 z-10">
+            <h1 className="text-white text-3xl text-center font-bold">TalkConnect</h1>
+        </header>
+    );
+}
+
+export default Header;
