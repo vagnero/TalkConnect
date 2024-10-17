9799022707680d390152d6a225d7a1841f30ea45
vagnero <vagnerimperador16@gmail.com>
2024-10-17 20:49:32 -0300

    correcao para buildar aplicacao

--- components/MessageList.tsx
+++ components/MessageList.tsx
@@ -1,5 +1,5 @@
 // components/MessageList.tsx
-import React, { useEffect, useState, useRef  } from 'react';
+import React, { useEffect, useState  } from 'react';
 import axios from 'axios';
 import { Client } from '@stomp/stompjs';
 
