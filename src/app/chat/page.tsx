67d2f480c72912d4a9cfeeebedfb819a990ab599
vagnero <vagnerimperador16@gmail.com>
2024-10-18 14:28:12 -0300

    teste

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 // pages/index.tsx
 import React, { useEffect, useRef, useState } from 'react';
 import MessageList from '../../../components/MessageList';
@@ -7,19 +6,18 @@ import Header from '../../../components/Header';
 import CallAction from '../../../components/CallActionProps';
 import FriendListToggle from '../../../components/FriendListToggle';
 
-
 const Home: React.FC = () => {
   const [selectedFriendId, setSelectedFriendId] = useState<number | null>(null);
   const [incomingCall, setIncomingCall] = useState<boolean>(false); // Estado para chamadas recebidas
   const [userId, setUserId] = useState<string | null>(null); // Estado para armazenar o userId
+  const [isOnCall, setIsOnCall] = useState<boolean>(false); // Adiciona o estado de "em chamada"
 
   const sipClientRef = useRef<SipClient | null>(null); // Referência da classe SipClient
 
-   // useEffect para garantir que o código é executado no lado do cliente
-   useEffect(() => {
+  // useEffect para garantir que o código é executado no lado do cliente
+  useEffect(() => {
     if (typeof window !== 'undefined') {
       const storedUserId = localStorage.getItem('userId');
-      console.log("ID DO USUARIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO: " + storedUserId);
       setUserId(storedUserId); // Armazena o userId no estado
     }
   }, []);
@@ -27,30 +25,28 @@ const Home: React.FC = () => {
   useEffect(() => {
     if (userId) {
       sipClientRef.current = new SipClient(userId, '1234', () => {
-        setIncomingCall(true);
+        setIncomingCall(true); // Configura o estado quando há uma chamada recebida
       });
     }
   }, [userId]);
-  const handleFriendSelect = (friendId: number) => {
 
+  const handleFriendSelect = (friendId: number) => {
     setSelectedFriendId(friendId);
   };
-  
 
-  // Função para ligar para um amigo
   const handleCallFriend = (friendId: number) => {
     if (sipClientRef.current) {
       sipClientRef.current.makeCall(friendId.toString()); // Faz uma chamada SIP para o ID do amigo
+      setIsOnCall(true); // Configura o estado para "em chamada"
     }
   };
 
-  // Função para aceitar a chamada
   const handleAcceptCall = () => {
     sipClientRef.current?.answerCall();
     setIncomingCall(false); // Reseta o estado da chamada recebida
+    setIsOnCall(true); // Configura o estado para "em chamada"
   };
 
-  // Função para recusar a chamada
   const handleRejectCall = () => {
     sipClientRef.current?.rejectCall();
     setIncomingCall(false); // Reseta o estado da chamada recebida
@@ -58,17 +54,14 @@ const Home: React.FC = () => {
 
   const handleEndCall = () => {
     sipClientRef.current?.endCall();
+    setIsOnCall(false); // Reseta o estado quando a chamada terminar
   };
-  
-
 
   return (
     <div className="flex flex-col h-screen">
-      {/* Certifique-se de que o Header tenha altura fixa */}
       <Header />
-      {/* Adiciona padding no topo igual à altura do Header */}
-      <div className="flex flex-grow pt-16"> {/* pt-16 ajusta o conteúdo abaixo do header */}
-      <FriendListToggle 
+      <div className="flex flex-grow pt-16">
+        <FriendListToggle 
           onFriendSelect={handleFriendSelect} 
           onCallFriend={handleCallFriend}
         />
@@ -79,17 +72,27 @@ const Home: React.FC = () => {
             <p className="text-xl text-gray-600">Selecione um amigo para ver as mensagens</p>
           </div>
         )}
-   
       </div>
-               {/* Se houver uma chamada recebida, exibir os botões */}
-         {/* Componente CallAction para gerenciar chamadas */}
-         <CallAction
+
+      <CallAction
         incomingCall={incomingCall}
         onAccept={handleAcceptCall}
         onReject={handleRejectCall}
         onEndCall={handleEndCall}
       />
+
+      {isOnCall && (
+        <div className="absolute inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 text-white">
+          <div className="text-center">
+            <h2 className="text-2xl mb-4">Em chamada...</h2>
+            <button onClick={handleEndCall} className="bg-red-500 text-white p-2 rounded">
+              Finalizar Chamada
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
 export default Home;
--- src/app/jssip/SipCliente.ts
+++ src/app/jssip/SipCliente.ts
@@ -1,4 +1,3 @@
-// SipClient.ts
 import JsSIP from 'jssip';
 import { RTCSessionEvent, UA } from 'jssip/lib/UA';
 import { RTCSession } from 'jssip/lib/RTCSession';
@@ -44,6 +43,15 @@ class SipClient {
         // Chama o callback para notificar o componente sobre a chamada recebida
         this.onIncomingCall(); // <<< ADICIONE ESSA LINHA
       }
+
+      // Configura o evento de áudio para sessões de saída
+      if (session.direction === 'outgoing') {
+        session.connection.addEventListener('track', (event: RTCTrackEvent) => {
+          const audioElement = document.createElement('audio');
+          audioElement.srcObject = event.streams[0]; // Acessa o primeiro stream
+          audioElement.play();
+        });
+      }
     });
   }
 
@@ -82,79 +90,53 @@ class SipClient {
     this.ua.call(`sip:${targetUri}@${domain}`, options);
   }
 
-    // Método público para aceitar chamadas
-    public answerCall() {
-      if (this.currentSession) {
-        const options = {
-          mediaConstraints: { audio: true, video: false },
-          rtcConfiguration: {
-            iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-          },
-        };
-  
-        this.currentSession.answer(options);
-  
-        this.currentSession.connection.addEventListener('track', (event: RTCTrackEvent) => {
-          const audioElement = document.createElement('audio');
-          audioElement.srcObject = event.streams[0]; // Access the first stream
-          audioElement.play();
-        });
-  
-        this.currentSession.on('ended', () => {
-          console.log('Call ended');
-          this.currentSession = null; // Limpa a sessão quando a chamada terminar
-        });
-  
-        this.currentSession.on('failed', () => {
-          console.log('Call failed');
-          this.currentSession = null; // Limpa a sessão se a chamada falhar
-        });
-      }
-    }
-  
-    // Método público para rejeitar chamadas
-    public rejectCall() {
-      if (this.currentSession) {
-        this.currentSession.terminate(); // Encerra a chamada
-        this.currentSession = null; // Limpa a sessão após a rejeição
-        console.log('Call rejected');
-      }
-    }
-    public endCall() {
-      if (this.currentSession) {
-        this.currentSession.terminate(); // Encerra a chamada em andamento
-        console.log('Call terminated');
-        this.currentSession = null; // Limpa a sessão após a chamada ser finalizada
-      }
+  // Método público para aceitar chamadas
+  public answerCall() {
+    if (this.currentSession) {
+      const options = {
+        mediaConstraints: { audio: true, video: false },
+        rtcConfiguration: {
+          iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+        },
+      };
+
+      this.currentSession.answer(options);
+
+      // Adicionar o listener de áudio para chamadas recebidas
+      this.currentSession.connection.addEventListener('track', (event: RTCTrackEvent) => {
+        const audioElement = document.createElement('audio');
+        audioElement.srcObject = event.streams[0]; // Acessa o primeiro stream
+        audioElement.play();
+      });
+
+      this.currentSession.on('ended', () => {
+        console.log('Call ended');
+        this.currentSession = null; // Limpa a sessão quando a chamada terminar
+      });
+
+      this.currentSession.on('failed', () => {
+        console.log('Call failed');
+        this.currentSession = null; // Limpa a sessão se a chamada falhar
+      });
     }
   }
-  
 
-  // // Método para gerenciar chamadas recebidas
-  // private handleIncomingCall(session: RTCSession) {
-  //   const options = {
-  //     mediaConstraints: { audio: true, video: false },
-  //     rtcConfiguration: {
-  //       iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-  //     },
-  //   };
+  // Método público para rejeitar chamadas
+  public rejectCall() {
+    if (this.currentSession) {
+      this.currentSession.terminate(); // Encerra a chamada
+      this.currentSession = null; // Limpa a sessão após a rejeição
+      console.log('Call rejected');
+    }
+  }
 
-  //   session.answer(options);
-
-  //   session.connection.addEventListener('track', (event: RTCTrackEvent) => {
-  //     const audioElement = document.createElement('audio');
-  //     audioElement.srcObject = event.streams[0]; // Access the first stream
-  //     audioElement.play();
-  //   });
-
-  //   session.on('ended', () => {
-  //     console.log('Call ended');
-  //   });
-
-  //   session.on('failed', () => {
-  //     console.log('Call failed');
-  //   });
-  // }
-
+  public endCall() {
+    if (this.currentSession) {
+      this.currentSession.terminate(); // Encerra a chamada em andamento
+      console.log('Call terminated');
+      this.currentSession = null; // Limpa a sessão após a chamada ser finalizada
+    }
+  }
+}
 
 export default SipClient;
