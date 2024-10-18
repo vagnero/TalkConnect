5ff39d00220b63c4e0f694ffff301af1d0faa504
vagnero <vagnerimperador16@gmail.com>
2024-10-17 22:25:36 -0300

    voltando novamente

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   // basePath: '/app',  // Aqui você define o caminho base para o Next.js
+    basePath: '/app',  // Aqui você define o caminho base para o Next.js
   };
 
 export default nextConfig;
